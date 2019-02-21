c1e3cb01eadfadaeae4e0048cf0fbfdd37231c80
sawyer <python.craver@gmail.com>
2019-02-21 15:56:23 +0545

    customize model admin

--- apps/ewallet/admin.py
+++ apps/ewallet/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from datetime import date
 
 from .models import Profile
 
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('address', 'dob', 'mobile', 'country', 'gender', 'profession', 'get_age')
+	list_display_links = ('address', 'mobile')
+	list_editable = ('dob', 'country')
+	list_filter = ('gender',)
+	search_fields = ('address', 'mobile', 'profession')
+	# fields = ['mobile', 'address',( 'dob', 'gender', 'country', 'profession')]
+
+	# fieldsets = (
+	# 	(None, {
+	# 		'fields': ('address', 'dob', 'mobile', 'country')	
+	# 	}),
+	# 	('Availability', {
+	# 		'fields':('gender', 'profession')	
+	# 	})
+	# )
+
+	def get_age(self, instance):
+		age = date.today() - instance.dob
+		return(age.days//365.25)
+
+	get_age.short_description = 'Age'
+
