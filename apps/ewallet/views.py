a1a7462cfabd537ccebbb913dd297edf8f39ba54
sawyer <python.craver@gmail.com>
2019-02-21 18:02:34 +0545

    function based views

--- apps/ewallet/views.py
+++ apps/ewallet/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse
+
+
+def home(request):
+	context = {
+		'title' : 'Home Page',
+		'name' : 'Roshan Maharjan' 
+	}
+	return render(request, 'home.html', context)
+
+def contact(request):
+	return HttpResponse('Contact us at: 01:4333234')
--- my_project/urls.py
+++ my_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.ewallet.views import home, contact
 urlpatterns = [
+	path('', home),
+	path('contact/', contact),
     path('admin/', admin.site.urls),
 ]
